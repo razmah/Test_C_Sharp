4d7607916458029ce28fe08187713a4795a2c399
Nikita Razmahov <nikita.razmahov@yandex.ru>
2023-11-01 23:29:06 +0300

    add method GetArrayThreeSimvol in Program.cs

--- Program.cs
+++ Program.cs
@@ -16,3 +16,27 @@ string[] GetArrayStringConsole (string consoleText)
     arrayText = consoleText.Split(",");
     return arrayText;
 }
+
+string[] GetArrayThreeSimvol (string[] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            temp++;
+        }
+    }
+    string[] arrayThreeSimvol = new string[temp];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
+}
